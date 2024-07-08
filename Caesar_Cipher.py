58a57cc28fbe66c10bd8819a9cee94d5f8f796d0
Luis Hasbun <41136478+lhasbun@users.noreply.github.com>
2024-07-07 21:02:32 -0500

    Added replayability functionality and ASCII artwork

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -100,6 +100,26 @@ def main():
 
     while inSession:
 
+        logo = """           
+            ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+           a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+           8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+           "8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+            `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+                                      88                                 
+                       88             88                                 
+                                      88                                 
+            ,adPPYba,  88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+            a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+            8b         88 88       d8 88       88 8PP""""""" 88          
+            "8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+            `"Ybbd8"'  88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+                          88                                             
+                          88           
+            """
+
+        print(logo)
+
         direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
         if direction.lower() == 'encode' or direction.lower() == 'decode':
@@ -111,15 +131,19 @@ def main():
 
             print(f'The {direction.lower()}d text is: {encoded}')
 
-            inSession = False
+            restart = input("Would you like to go again? Y/N\n")
+
+            if restart.lower() == 'y':
+                os.system('cls')
+                continue
+            elif restart.lower() == 'n':
+                inSession = False
+            else:
+                print('INVALID INPUT: Please type in ''Y'' or ''N''')
 
         else:
 
-            print('INVALID INPUT: Please type in ''encode'' or ''decode''')
-        
-    else:
-
-        exit
-    
+            print('INVALID INPUT: Please type in ''encode'' or ''decode''')    
+  
 if __name__=="__main__": 
     main()
